0fc26d68c6999452896612762d12e8ede6e45cc4
Manuel Palumbo <manuel.palumbo@email.com>
2024-05-30 22:30:25 +0200

    versioning: updated to 0.0.3-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
-version = findProperty("tag") ?: "0.0.2-SNAPSHOT"
+version = findProperty("tag") ?: "0.0.3-SNAPSHOT"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
