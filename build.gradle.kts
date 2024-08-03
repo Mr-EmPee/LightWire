c04faa75286b3f21fc69f3151652c315c828518b
Manuel Palumbo <manuel.palumbo@email.com>
2024-08-03 21:57:16 +0200

    versioning: bump to 0.0.6-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
   id("com.vanniktech.maven.publish") version "0.28.0"
 }
 
-version = findProperty("tag") ?: "0.0.5-SNAPSHOT"
+version = findProperty("tag") ?: "0.0.6-SNAPSHOT"
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(11))
 
